263f70bab48fad1113c44a2eceef93cb72215042
zhangxiu <zhangxiu@qq.com>
2014-08-14 14:26:52 +0800

    s

--- ZJdemo/ZJdemo/AppDelegate.m
+++ ZJdemo/ZJdemo/AppDelegate.m
@@ -14,14 +14,7 @@
 {
     // Override point for customization after application launch.
     return YES;
-    //真的快搞定了
-    //老赵你真是个天才
-    //真的快搞定了
-    //老赵你真是个天才
-    //真的快搞定了
-    //老赵你真是个天才
-    //真的快搞定了
-    //老赵你真是个天才
+ 
     
 }
 							
