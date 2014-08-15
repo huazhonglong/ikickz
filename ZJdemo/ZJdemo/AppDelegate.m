40c3f765073ed6fcf2d6c3b6c85e12eded40cec2
zhao <zjdwy@126.com>
2014-08-15 10:22:24 +0800

    s

--- ZJdemo/ZJdemo/AppDelegate.m
+++ ZJdemo/ZJdemo/AppDelegate.m
@@ -15,8 +15,12 @@
     // Override point for customization after application launch.
     return YES;
  //dlskafjdsalkfjldsafjds lkafldsa
+    NSLog(@"123");
+    NSLog(@"123");
+    NSLog(@"123");
+
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- ZJdemo/ZJdemo/ViewController.m
+++ ZJdemo/ZJdemo/ViewController.m
@@ -18,6 +18,9 @@
 {
     [super viewDidLoad];
     NSLog(@"123");
+    NSLog(@"123");
+    NSLog(@"123");
+
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
